f846e6987a4852fed7f5f3151486cab166103290
lukevance <lukevance90@gmail.com>
2015-11-13 15:29:58 -0700

    finished color pallette and improved eventlistening

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ window.onload = function (){
   //
   var build = function(){
     //later add funcitonality for dynamic changing of number
-    var rowNumber = 10;
-    var colNUmber = 10;
+    var rowNumber = 15;
+    var colNUmber = 15;
     //based on number add divs to canvas
     var canvas = document.getElementById('canvas');
 
@@ -44,20 +44,65 @@ window.onload = function (){
     }
   }();
 
-	function paint(){
-    var boxes = document.querySelectorAll('.canvasBox');
-    Object.keys(boxes).forEach(function(elem, idx){
-      var pixel = boxes[idx]; pixel.addEventListener('click', function(){
-        event.target.style.backgroundColor = 'red';
-      })
-    });
+  //set default paintBrush color
+  var paintBrush = 'red';
+
+	var paint = function(elem){
+    // var boxes = document.querySelectorAll('.canvasBox');
+    // Object.keys(boxes).forEach(function(elem, idx){
+    //   var pixel = boxes[idx];
+    //   pixel.addEventListener('click', function(){
+    //     event.target.style.backgroundColor = 'red';
+    //   })
+    // });
+    elem.style.backgroundColor = paintBrush;
   };
-  paint();
 
   // pallette handler function
-  // get pallette source
-    // set standard color to pallette source color
+  var palletteBuilder = function(){
+    // number of colors in pallete
+    // var pallNumber = 5;
+    //
+    // var pallRow = document.getElementById('pallete');
 
 
-    // make new standard color available to paint(); function
+
+
+
+
+      // make new standard color available to paint(); function
+  }();
+
+  var pallettePicker = function (elem) {
+    // var palBoxes = document.querySelectorAll('.pallBox');
+    // Object.keys(palBoxes).forEach(function(elem, idx) {
+    //   // get pallette destination
+    //   var pixelPall = palBoxes[idx];
+    //   console.log(pixelPall);
+    //   // on click set standard color to pallette source color
+    //   pixelPall.addEventListener('click', function(){
+    elem.style.border = "solid thick black";
+    paintBrush = elem.style.backgroundColor;
+    //   });
+
+    // });
+  };
+
+
+addEventListener('click', function(event){
+  var clickedEl = event.target;
+  var arrayOfElems = clickedEl.className.split(' ');
+  for (var i = 0; i < arrayOfElems.length; i++){
+    if (arrayOfElems[i] === 'canvasBox'){
+      paint(clickedEl);
+    } else if (arrayOfElems[i] === 'pallBox') {
+      var palBoxes = document.getElementsByClassName('pallBox')
+      Object.keys(palBoxes).forEach(function(elem, idx){
+        palBoxes[idx].style.border = "solid thin black";
+      });
+      pallettePicker(clickedEl);
+    }
+  }
+})
+
 };
